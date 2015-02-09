4846f8b4b10338eae76cc87ba36ac88ff2f40108
Matthäus G. Chajdas <dev@anteru.net>
2015-02-09 12:35:36 +0100

    Refactor the code, add BC4/5 and rename everything to BC*.

--- decompress.c
+++ decompress.c
@@ -1,17 +1,18 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <assert.h>
 
 /*
 DXT1/DXT3/DXT5 texture decompression
 
 The original code is from Benjamin Dobell, see below for details. Compared to
-the original this one adds DXT3 decompression, is valid C89, and is x64 
-compatible as it uses fixed size integers everywhere. It also uses a different
-PackRGBA order.
+the original the code is now valid C89, has support for 64-bit architectures
+and has been refactored. It also has support for additional formats and uses
+a different PackRGBA order.
 
 ---
 
-Copyright (c) 2012, Matth‰us G. "Anteru" Chajdas (http://anteru.net)
+Copyright (c) 2012 - 2015 Matth√§us G. "Anteru" Chajdas (http://anteru.net)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in 
@@ -60,10 +61,55 @@ static uint32_t PackRGBA (uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 	return r | (g << 8) | (b << 16) | (a << 24);
 }
 
-static void DecompressBlockDXT1Internal (const uint8_t* block,
-	uint32_t* output,
-	uint32_t outputStride,
-	const uint8_t* alphaValues)
+static float Int8ToFloat_SNORM (const uint8_t input)
+{
+	return (float)((int8_t)input) / 127.0f;
+}
+
+static float Int8ToFloat_UNORM (const uint8_t input)
+{
+	return (float)input / 255.0f;
+}
+
+/**
+Decompress a BC 16x3 index block stored as
+h g f e
+d c b a
+p o n m
+l k j i
+
+Bits packed as
+
+| h | g | f | e | d | c | b | a | // Entry
+|765 432 107 654 321 076 543 210| // Bit
+|0000000000111111111112222222222| // Byte
+
+into 16 8-bit indices.
+*/
+static void Decompress16x3bitIndices (const uint8_t* packed, uint8_t* unpacked)
+{
+	uint32_t tmp, block, i;
+
+	for (block = 0; block < 2; ++block) {
+		tmp = 0;
+
+		// Read three bytes
+		for (i = 0; i < 3; ++i) {
+			tmp |= ((uint32_t)packed [i]) << (i * 8);
+		}
+
+		// Unpack 8x3 bit from last 3 byte block
+		for (i = 0; i < 8; ++i) {
+			unpacked [i] = (tmp >> (i*3)) & 0x7;
+		}
+
+		packed += 3;
+		unpacked += 8;
+	}
+}
+
+static void DecompressBlockBC1Internal (const uint8_t* block,
+	unsigned char* output, uint32_t outputStride, const uint8_t* alphaValues)
 {
 	uint32_t temp, code;
 
@@ -117,7 +163,7 @@ static void DecompressBlockDXT1Internal (const uint8_t* block,
 					break;
 				}
 
-				output [j*outputStride + i] = finalColor;
+				*(uint32_t*)(output + j*outputStride + i * sizeof (uint32_t)) = finalColor;
 			}
 		}
 	} else {
@@ -146,24 +192,23 @@ static void DecompressBlockDXT1Internal (const uint8_t* block,
 					break;
 				}
 
-				output [j*outputStride + i] = finalColor;
+				*(uint32_t*)(output + j*outputStride + i * sizeof (uint32_t)) = finalColor;
 			}
 		}
 	}
 }
 
 /*
-void DecompressBlockDXT1(): Decompresses one block of a DXT1 texture and stores the resulting pixels at the appropriate offset in 'image'.
+Decompresses one block of a BC1 (DXT1) texture and stores the resulting pixels at the appropriate offset in 'image'.
 
 uint32_t x:						x-coordinate of the first pixel in the block.
 uint32_t y:						y-coordinate of the first pixel in the block.
-uint32_t width: 				width of the texture being decompressed.
-const uint8_t *blockStorage:	pointer to the block to decompress.
-uint32_t *image:				pointer to image where the decompressed pixel data should be stored.
-*/ 
-void DecompressBlockDXT1(uint32_t x, uint32_t y, uint32_t width,
-	const uint8_t* blockStorage,
-	uint32_t* image)
+uint32_t stride:				stride of a scanline in bytes.
+const uint8_t* blockStorage:	pointer to the block to decompress.
+uint32_t* image:				pointer to image where the decompressed pixel data should be stored.
+*/
+void DecompressBlockBC1 (uint32_t x, uint32_t y, uint32_t stride,
+	const uint8_t* blockStorage, unsigned char* image)
 {
 	static const uint8_t const_alpha [] = {
 		255, 255, 255, 255,
@@ -172,26 +217,24 @@ void DecompressBlockDXT1(uint32_t x, uint32_t y, uint32_t width,
 		255, 255, 255, 255
 	};
 
-	DecompressBlockDXT1Internal (blockStorage,
-		image + x + (y * width), width, const_alpha);
+	DecompressBlockBC1Internal (blockStorage,
+		image + x * sizeof (uint32_t) + (y * stride), stride, const_alpha);
 }
 
 /*
-void DecompressBlockDXT5(): Decompresses one block of a DXT5 texture and stores the resulting pixels at the appropriate offset in 'image'.
+Decompresses one block of a BC3 (DXT5) texture and stores the resulting pixels at the appropriate offset in 'image'.
 
 uint32_t x:						x-coordinate of the first pixel in the block.
 uint32_t y:						y-coordinate of the first pixel in the block.
-uint32_t width: 				width of the texture being decompressed.
+uint32_t stride:				stride of a scanline in bytes.
 const uint8_t *blockStorage:	pointer to the block to decompress.
 uint32_t *image:				pointer to image where the decompressed pixel data should be stored.
-*/ 
-void DecompressBlockDXT5(uint32_t x, uint32_t y, uint32_t width,
-	const uint8_t* blockStorage, uint32_t* image)
+*/
+void DecompressBlockBC3 (uint32_t x, uint32_t y, uint32_t stride,
+	const uint8_t* blockStorage, unsigned char* image)
 {
 	uint8_t alpha0, alpha1;
-	const uint8_t* bits;
-	uint32_t alphaCode1;
-	uint16_t alphaCode2;
+	uint8_t alphaIndices [16];
 
 	uint16_t color0, color1;
 	uint8_t r0, g0, b0, r1, g1, b1;
@@ -203,44 +246,35 @@ void DecompressBlockDXT5(uint32_t x, uint32_t y, uint32_t width,
 	alpha0 = *(blockStorage);
 	alpha1 = *(blockStorage + 1);
 
-	bits = blockStorage + 2;
-	alphaCode1 = bits[2] | (bits[3] << 8) | (bits[4] << 16) | (bits[5] << 24);
-	alphaCode2 = bits[0] | (bits[1] << 8);
+	Decompress16x3bitIndices (blockStorage + 2, alphaIndices);
 
 	color0 = *(const uint16_t*)(blockStorage + 8);
-	color1 = *(const uint16_t*)(blockStorage + 10);	
+	color1 = *(const uint16_t*)(blockStorage + 10);
 
 	temp = (color0 >> 11) * 255 + 16;
-	r0 = (uint8_t)((temp/32 + temp)/32);
+	r0 = (uint8_t)((temp / 32 + temp) / 32);
 	temp = ((color0 & 0x07E0) >> 5) * 255 + 32;
-	g0 = (uint8_t)((temp/64 + temp)/64);
+	g0 = (uint8_t)((temp / 64 + temp) / 64);
 	temp = (color0 & 0x001F) * 255 + 16;
-	b0 = (uint8_t)((temp/32 + temp)/32);
+	b0 = (uint8_t)((temp / 32 + temp) / 32);
 
 	temp = (color1 >> 11) * 255 + 16;
-	r1 = (uint8_t)((temp/32 + temp)/32);
+	r1 = (uint8_t)((temp / 32 + temp) / 32);
 	temp = ((color1 & 0x07E0) >> 5) * 255 + 32;
-	g1 = (uint8_t)((temp/64 + temp)/64);
+	g1 = (uint8_t)((temp / 64 + temp) / 64);
 	temp = (color1 & 0x001F) * 255 + 16;
-	b1 = (uint8_t)((temp/32 + temp)/32);
+	b1 = (uint8_t)((temp / 32 + temp) / 32);
 
 	code = *(const uint32_t*)(blockStorage + 12);
 
 	for (j = 0; j < 4; j++) {
 		for (i = 0; i < 4; i++) {
 			uint8_t finalAlpha;
-			int alphaCode, alphaCodeIndex;
+			int alphaCode;
 			uint8_t colorCode;
 			uint32_t finalColor;
 
-			alphaCodeIndex = 3*(4*j+i);
-			if (alphaCodeIndex <= 12) {
-				alphaCode = (alphaCode2 >> alphaCodeIndex) & 0x07;
-			} else if (alphaCodeIndex == 15) {
-				alphaCode = (alphaCode2 >> 15) | ((alphaCode1 << 1) & 0x06);
-			} else /* alphaCodeIndex >= 18 && alphaCodeIndex <= 45 */ {
-				alphaCode = (alphaCode1 >> (alphaCodeIndex - 16)) & 0x07;
-			}
+			alphaCode = alphaIndices [4 * j + i];
 
 			if (alphaCode == 0) {
 				finalAlpha = alpha0;
@@ -248,69 +282,201 @@ void DecompressBlockDXT5(uint32_t x, uint32_t y, uint32_t width,
 				finalAlpha = alpha1;
 			} else {
 				if (alpha0 > alpha1) {
-					finalAlpha = (uint8_t)(((8-alphaCode)*alpha0 + (alphaCode-1)*alpha1)/7);
+					finalAlpha = (uint8_t)(((8 - alphaCode)*alpha0 + (alphaCode - 1)*alpha1) / 7);
 				} else {
 					if (alphaCode == 6) {
 						finalAlpha = 0;
 					} else if (alphaCode == 7) {
 						finalAlpha = 255;
 					} else {
-						finalAlpha = (uint8_t)(((6-alphaCode)*alpha0 + (alphaCode-1)*alpha1)/5);
+						finalAlpha = (uint8_t)(((6 - alphaCode)*alpha0 + (alphaCode - 1)*alpha1) / 5);
 					}
 				}
 			}
 
-			colorCode = (code >> 2*(4*j+i)) & 0x03; 
+			colorCode = (code >> 2 * (4 * j + i)) & 0x03;
 			finalColor = 0;
 
 			switch (colorCode) {
 			case 0:
-				finalColor = PackRGBA(r0, g0, b0, finalAlpha);
+				finalColor = PackRGBA (r0, g0, b0, finalAlpha);
 				break;
 			case 1:
-				finalColor = PackRGBA(r1, g1, b1, finalAlpha);
+				finalColor = PackRGBA (r1, g1, b1, finalAlpha);
 				break;
 			case 2:
-				finalColor = PackRGBA((2*r0+r1)/3, (2*g0+g1)/3, (2*b0+b1)/3, finalAlpha);
+				finalColor = PackRGBA ((2 * r0 + r1) / 3, (2 * g0 + g1) / 3, (2 * b0 + b1) / 3, finalAlpha);
 				break;
 			case 3:
-				finalColor = PackRGBA((r0+2*r1)/3, (g0+2*g1)/3, (b0+2*b1)/3, finalAlpha);
+				finalColor = PackRGBA ((r0 + 2 * r1) / 3, (g0 + 2 * g1) / 3, (b0 + 2 * b1) / 3, finalAlpha);
 				break;
 			}
 
-			image [i + x + (width* (y+j))] = finalColor; 
+
+			*(uint32_t*)(image + sizeof (uint32_t) * (i + x) + (stride * (y + j))) = finalColor;
 		}
 	}
 }
 
 /*
-void DecompressBlockDXT3(): Decompresses one block of a DXT3 texture and stores the resulting pixels at the appropriate offset in 'image'.
+Decompresses one block of a BC2 (DXT3) texture and stores the resulting pixels at the appropriate offset in 'image'.
 
 uint32_t x:						x-coordinate of the first pixel in the block.
 uint32_t y:						y-coordinate of the first pixel in the block.
-uint32_t height:				height of the texture being decompressed.
+uint32_t stride:				stride of a scanline in bytes.
 const uint8_t *blockStorage:	pointer to the block to decompress.
 uint32_t *image:				pointer to image where the decompressed pixel data should be stored.
-*/ 
-void DecompressBlockDXT3(uint32_t x, uint32_t y, uint32_t width,
-	const uint8_t* blockStorage,
-	uint32_t* image)
+*/
+void DecompressBlockBC2 (uint32_t x, uint32_t y, uint32_t stride,
+	const uint8_t* blockStorage, unsigned char* image)
 {
 	int i;
 
 	uint8_t alphaValues [16] = { 0 };
 
 	for (i = 0; i < 4; ++i) {
-		const uint16_t* alphaData = (const uint16_t*) (blockStorage);
+		const uint16_t* alphaData = (const uint16_t*)(blockStorage);
 
-		alphaValues [i*4 + 0] = (((*alphaData) >> 0) & 0xF ) * 17;
-		alphaValues [i*4 + 1] = (((*alphaData) >> 4) & 0xF ) * 17;
-		alphaValues [i*4 + 2] = (((*alphaData) >> 8) & 0xF ) * 17;
-		alphaValues [i*4 + 3] = (((*alphaData) >> 12) & 0xF) * 17;
+		alphaValues [i * 4 + 0] = (((*alphaData) >> 0) & 0xF) * 17;
+		alphaValues [i * 4 + 1] = (((*alphaData) >> 4) & 0xF) * 17;
+		alphaValues [i * 4 + 2] = (((*alphaData) >> 8) & 0xF) * 17;
+		alphaValues [i * 4 + 3] = (((*alphaData) >> 12) & 0xF) * 17;
 
 		blockStorage += 2;
 	}
 
-	DecompressBlockDXT1Internal (blockStorage,
-		image + x + (y * width), width, alphaValues);
+	DecompressBlockBC1Internal (blockStorage,
+		image + x * sizeof (uint32_t) + (y * stride), stride, alphaValues);
+}
+
+enum BC4Mode
+{
+	BC4_UNORM = 0,
+	BC4_SNORM = 1
+};
+
+enum BC5Mode
+{
+	BC5_UNORM = 0,
+	BC5_SNORM = 1
+};
+
+static void DecompressBlockBC4Internal (
+	const uint8_t* block, unsigned char* output,
+	uint32_t outputStride, const float* colorTable)
+{
+	uint8_t indices [16];
+	int x, y;
+
+	Decompress16x3bitIndices (block + 2, indices);
+
+	for (y = 0; y < 4; ++y) {
+		for (x = 0; x < 4; ++x) {
+			*(float*)(output + x * sizeof (float)) = colorTable [indices [y*4 + x]];
+		}
+
+		output += outputStride;
+	}
+}
+
+/*
+Decompresses one block of a BC4 texture and stores the resulting pixels at the appropriate offset in 'image'.
+
+uint32_t x:						x-coordinate of the first pixel in the block.
+uint32_t y:						y-coordinate of the first pixel in the block.
+uint32_t stride:				stride of a scanline in bytes.
+const uint8_t* blockStorage:	pointer to the block to decompress.
+float* image:					pointer to image where the decompressed pixel data should be stored.
+*/
+void DecompressBlockBC4 (uint32_t x, uint32_t y, uint32_t stride, enum BC4Mode mode,
+	const uint8_t* blockStorage, unsigned char* image)
+{
+	float colorTable [8];
+	float r0, r1;
+
+	if (mode == BC4_UNORM) {
+		r0 = Int8ToFloat_UNORM (blockStorage [0]);
+		r1 = Int8ToFloat_UNORM (blockStorage [1]);
+
+		colorTable [0] = r0;
+		colorTable [1] = r1;
+
+		if (r0 > r1) {
+			// 6 interpolated color values
+			colorTable [2] = (6*r0 + 1*r1)/7.0f; // bit code 010
+			colorTable [3] = (5*r0 + 2*r1)/7.0f; // bit code 011
+			colorTable [4] = (4*r0 + 3*r1)/7.0f; // bit code 100
+			colorTable [5] = (3*r0 + 4*r1)/7.0f; // bit code 101
+			colorTable [6] = (2*r0 + 5*r1)/7.0f; // bit code 110
+			colorTable [7] = (1*r0 + 6*r1)/7.0f; // bit code 111
+		} else {
+			// 4 interpolated color values
+			colorTable [2] = (4*r0 + 1*r1)/5.0f; // bit code 010
+			colorTable [3] = (3*r0 + 2*r1)/5.0f; // bit code 011
+			colorTable [4] = (2*r0 + 3*r1)/5.0f; // bit code 100
+			colorTable [5] = (1*r0 + 4*r1)/5.0f; // bit code 101
+			colorTable [6] = 0.0f;               // bit code 110
+			colorTable [7] = 1.0f;               // bit code 111
+		}
+	} else if (mode == BC4_SNORM) {
+		r0 = Int8ToFloat_SNORM (blockStorage [0]);
+		r1 = Int8ToFloat_SNORM (blockStorage [1]);
+
+		colorTable [0] = r0;
+		colorTable [1] = r1;
+
+		if (r0 > r1) {
+		  // 6 interpolated color values
+		  colorTable [2] = (6*r0 + 1*r1)/7.0f; // bit code 010
+		  colorTable [3] = (5*r0 + 2*r1)/7.0f; // bit code 011
+		  colorTable [4] = (4*r0 + 3*r1)/7.0f; // bit code 100
+		  colorTable [5] = (3*r0 + 4*r1)/7.0f; // bit code 101
+		  colorTable [6] = (2*r0 + 5*r1)/7.0f; // bit code 110
+		  colorTable [7] = (1*r0 + 6*r1)/7.0f; // bit code 111
+		} else {
+		  // 4 interpolated color values
+		  colorTable [2] = (4*r0 + 1*r1)/5.0f; // bit code 010
+		  colorTable [3] = (3*r0 + 2*r1)/5.0f; // bit code 011
+		  colorTable [4] = (2*r0 + 3*r1)/5.0f; // bit code 100
+		  colorTable [5] = (1*r0 + 4*r1)/5.0f; // bit code 101
+		  colorTable [6] = -1.0f;              // bit code 110
+		  colorTable [7] =  1.0f;              // bit code 111
+		}
+	}
+
+	DecompressBlockBC4Internal (blockStorage,
+		image + x * sizeof (float) + (y * stride), stride, colorTable);
+}
+
+
+/*
+Decompresses one block of a BC5 texture and stores the resulting pixels at the appropriate offset in 'image'.
+
+uint32_t x:						x-coordinate of the first pixel in the block.
+uint32_t y:						y-coordinate of the first pixel in the block.
+uint32_t stride:				stride of a scanline in bytes.
+const uint8_t* blockStorage:	pointer to the block to decompress.
+float* image:					pointer to image where the decompressed pixel data should be stored.
+*/
+void DecompressBlockBC5 (uint32_t x, uint32_t y, uint32_t stride, enum BC5Mode mode,
+	const uint8_t* blockStorage, unsigned char* image)
+{
+	// We decompress the two channels separately and interleave them when
+	// writing to the output
+	float c0 [16];
+	float c1 [16];
+
+	int dx, dy;
+
+	DecompressBlockBC4 (0, 0, 4 * sizeof (float), (enum BC4Mode)mode, 
+		blockStorage, (unsigned char*)c0);
+	DecompressBlockBC4 (0, 0, 4 * sizeof (float), (enum BC4Mode)mode, 
+		blockStorage + 8, (unsigned char*)c1);
+
+	for (dy = 0; dy < 4; ++dy) {
+		for (dx = 0; dx < 4; ++dx) {
+			*(float*)(image + stride * y + (x + dx + 0) * sizeof (float) * 2) = c0 [dy * 4 + dx];
+			*(float*)(image + stride * y + (x + dx + 1) * sizeof (float) * 2) = c1 [dy * 4 + dx];
+		}
+	}
 }
